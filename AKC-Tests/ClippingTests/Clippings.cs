6e14990d74a3187ccaf19c538526e91b086923c4
DazEdword <darkouterheaven@gmail.com>
2016-11-20 17:29:27 +0000

    Trying first implementation of Specflow + Nunit.

---
+++ AKC-Tests/ClippingTests/Clippings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using ClippingManager;
+
+namespace AKCTests.ClippingTests
+{
+    [Binding]
+    public class ClippingsSteps
+    {
+        //Beware, we are testing static method of the class. 
+        Clipping clipping = new Clipping();
+        Clipping clip = null;
+
+        [When(@"I use a null clipping item in IsNullOrEmpty method\.")]
+        public void WhenIUseANullClippingItem_(Clipping clip) {
+            bool result = Clipping.IsNullOrEmpty(clip);
+        }
+
+        [Then(@"Class has to return ""(.*)""")]
+        public void ThenClassHasToReturn(Clipping clip) {
+            bool result = Clipping.IsNullOrEmpty(clip);
+        }
+    }
+}
