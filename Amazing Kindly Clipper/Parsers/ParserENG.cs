66b0fd8e6c038bdba5b78e79faf54679a3599f1e
Manuel Mesa Cano <manuelmesacano@gmail.com>
2016-11-30 21:14:36 +0100

    Various test harness are defined for the parser
    In order to work with legacy code we define some tests to fix the parser's behaviour and refactor the code. Uncomplete still in progress

---
+++ Amazing Kindly Clipper/Parsers/ParserENG.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClippingManager{
+    public class ParserENG:MyClippingsParser{
+        public string Sample { get; set; }
+
+
+        public void Parse(){
+
+            //ParseLine1(line, clipping);
+            //Parse(Sample);
+        }
+
+        public override void ParseLine2(string a, Clipping b, FormatType c) {
+           
+        }
+
+        public Clipping Result = new Clipping();
+
+    }
+}
