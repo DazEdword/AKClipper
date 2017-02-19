de9d83a3a98def7a01a92282780d3959d1867e70
Edword <eduardo@gottabegarabi.com>
2017-02-19 14:04:01 +0000

    Added navigation, updated content.

---
+++ AKCWeb/Controllers/AboutController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AKCWeb.Controllers {
+    public class AboutController : Controller {
+        public IActionResult Index() {
+            return View("About");
+        }
+    }
+}
--- AKCWeb/Controllers/HomeController.cs
+++ AKCWeb/Controllers/HomeController.cs
@@ -7,38 +7,5 @@ namespace AKCWeb.Controllers {
         public ViewResult Index() {
             return View("Index");
         }
-        
-        //Entry point. Action method or "action"
-        /*
-        public string Index() {
-            return "Hello World";
-        }
-        */
     }
 }
-
-/*
-//Entry point.
-public IActionResult Index() {
-    return View();
-}
-
-public string Index() {
-    return "Hello world!";
-}
-
-public IActionResult About() {
-    ViewData["Message"] = "Your application description page.";
-
-    return View();
-}
-
-public IActionResult Contact() {
-    ViewData["Message"] = "Your contact page.";
-
-    return View();
-}
-
-public IActionResult Error() {
-    return View();
-} */
