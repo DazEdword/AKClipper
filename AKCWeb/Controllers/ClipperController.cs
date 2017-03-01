d91b351490e63abdf89358f11ecc79fab9cfdc80
Edword <eduardo@gottabegarabi.com>
2017-03-01 10:30:38 +0000

    Fixed dependency issues, moved up to .NET 4.6.1 for better compatibility.

--- AKCWeb/Controllers/ClipperController.cs
+++ AKCWeb/Controllers/ClipperController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AKCCore;
 
 namespace AKCWeb.Controllers {
 
@@ -11,13 +12,13 @@ namespace AKCWeb.Controllers {
          * many other times, we'd better instantiate it with the ClipperController, on construction.
         */
 
-        //public ActionResult BrowseFile(string path, int lines = 39) {
-        //    ParserController parserController = InitParserController();
-        //    return Content(parserController.GeneratePreviewFromPath(path, lines));
-        //}
+        public ActionResult BrowseFile(string path, int lines = 39) {
+           ParserController parserController = InitParserController();
+            return Content(parserController.GeneratePreviewFromPath(path, lines));
+        }
 
-        //public ParserController InitParserController() {
-        //    return new ParserController();
-        //}
+        public ParserController InitParserController() {
+            return new ParserController();
+        }
     }
 }
