79aa9ccd94750188be96084c6d904077c75622b5
Edword <eduardo@gottabegarabi.com>
2017-03-05 20:08:57 +0000

    Experimented with client/side JS. Removed URL area (doesn't make sense on web app).

--- AKCWebCore/ViewComponents/ParserControllerViewComponent.cs
+++ AKCWebCore/ViewComponents/ParserControllerViewComponent.cs
@@ -1,18 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using AKCCore;
 
 namespace AKCWebCore.ViewComponents {
 
     public class ParserControllerViewComponent : ViewComponent {
-        private ParserController _parserController;
-
-        public ParserControllerViewComponent() {
-            _parserController = new ParserController();
-            System.Diagnostics.Debug.WriteLine("ParserController instanced");
-        }
-
+        //ViewComponent
         public IViewComponentResult Invoke() {
-            ParserController parserController = _parserController;
             return View("~/Views/Clipper/Main.cshtml");
         }
     }
