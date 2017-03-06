5fca222dda5c745866a3e66d39724bdbb6e3b297
Edword <eduardo@gottabegarabi.com>
2017-03-06 12:11:35 +0000

    Older/Experiment/Deprecate projects clean up.

--- AKCDesktop/Clippings/SortableClippingList.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.ObjectModel;
-
-namespace AKCDesktop {
-
-    public class SortableClippingList : ObservableCollection<Clipping> {
-        // Creating the Tasks collection in this fashion enables data binding from XAML.
-    }
-}
