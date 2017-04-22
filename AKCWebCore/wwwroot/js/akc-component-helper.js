feb7a006fb7ec28f9c53efb26b5f847208c1a376
Ed Garabito <ed.garabito@storystream.it>
2017-04-22 18:24:22 +0100

    Renamed client parsing helper with better name.

--- AKCWebCore/wwwroot/js/akc-component-helper.js
+++ AKCWebCore/wwwroot/js/akc-component-helper.js
@@ -12,6 +12,8 @@ window.onload = function () {
     document.getElementById('startParsingButton').onclick = (function() {
         sendParseRequest(undefined, undefined);
     })
+
+    highlightSelectedNavItem();
 };
 
 function sendParseRequest(content, language) {
@@ -89,4 +91,18 @@ function enableParsingButton() {
 
 function disableParsingButton() {
     document.getElementById("startParsingButton").disabled = true;
+}
+
+function highlightSelectedNavItem() {
+    $('#nav span a').click(function () {
+
+        //'unselect' all the rest
+
+        $('#nav span a').each(function (index, element) {
+            $(this).removeClass("current-page-item"); // set all links' style to unselected first
+        })
+
+        // $(this) is now the link user has clicked on
+        $(this).addClass('current-page-item'); // class you want to use for the link user clicked
+    });
 }
