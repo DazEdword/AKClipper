bf861adeead4cb1263f6f8327f05dff8bcc7a47c
Ed Garabito <eduardo@gottabegarabi.com>
2017-05-14 19:12:38 +0100

    Added more tests and mocking framework (Moq).

---
+++ AKCTests/Core/Clippings/DateParsingTests.cs
@@ -0,0 +1,22 @@
+﻿using AKCCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AKCTests.Core.Clippings {
+
+    internal class DateParsingTests {
+        [TestInitialize]
+        public void Init() {
+            //MyClippingsParser parser_eng = new MyClippingsParser();
+        }
+
+        [TestMethod]
+        public void Dummy() {
+
+        }
+
+        //[TestMethod]
+        //public void Dummy() {
+            
+        //}
+    }
+}
