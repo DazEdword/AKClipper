dbc671dc1f54b51605458995a16ef773b230e585
Ed Garabito <eduardo@gottabegarabi.com>
2017-05-20 16:15:25 +0100

    Implemented session-aware helper, started refactor.

--- AKCWebCore/Models/ParserWebHelper.cs
+++ AKCWebCore/Models/ParserWebHelper.cs
@@ -1,6 +1,10 @@
 ﻿using AKCCore;
+using System;
+using AKCWebCore.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
-
+using Newtonsoft.Json;
 
 namespace AKCWebCore.Models {
 
@@ -12,14 +16,43 @@ namespace AKCWebCore.Models {
         public string preview { get; set; }
         public List<Clipping> clippingData;
 
+        public ParserController parserController;
+        public const string helperKey = "ParserHelper";
 
         //Sync
         public ParserWebHelper() {
-                this.preview = "A preview of your text will appear here.";
-                this.language = "English";
-                this.reset = false;
-                this.clippingData = new List<Clipping>();
+            InitHelper();
         }
+
+        public void InitHelper() {
+            this.preview = "A preview of your text will appear here.";
+            this.language = "English";
+            this.reset = false;
+            this.clippingData = new List<Clipping>();
+            this.parserController = new ParserController();
+        }
+        
+        //Session-aware helper
+        public static ParserWebHelper GetHelper(IServiceProvider services) {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+            .HttpContext.Session;
+            ParserWebHelper helper = session?.GetJson<ParserWebHelper>(helperKey)
+            ?? new ParserWebHelper();
+            helper.Session = session;
+            return helper;
+        }
+
+        public void Save() {
+            Session.SetJson(helperKey, this);
+        }
+
+        public void Reset() {
+            Session.Remove(helperKey);
+            InitHelper();
+        }
+
+        [JsonIgnore]
+        public ISession Session { get; set; }
     }
 
 
