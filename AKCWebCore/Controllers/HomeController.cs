07abfbf30e5a3c9fcd1bc4fdb921fe3da6f7bb7b
Ed Garabito <eduardo@gottabegarabi.com>
2017-12-01 20:59:31 +0000

    Added epplus and support to .xlsx format (WIP).

--- AKCWebCore/Controllers/HomeController.cs
+++ AKCWebCore/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AKCWebCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NonFactors.Mvc.Grid;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
 
 namespace AKCWeb.Controllers {
 
     public class HomeController : Controller {
 
         public ParserWebHelper Helper;
+        private const string XlsxContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
         public HomeController(ParserWebHelper helper) {
             Helper = helper;
@@ -28,6 +34,11 @@ namespace AKCWeb.Controllers {
             return View("Index", Helper);
         }
 
+        [Route("home/export")]
+        public ActionResult ExportResults() {
+            return ExportGrid();
+        }
+
         [HttpPost]
         public IActionResult Parse(string content, string language) {
             if (content != null && content.Length > 0) {
@@ -39,6 +50,62 @@ namespace AKCWeb.Controllers {
                 //TODO Not found added just temporarily, return useful error message for user. 
                 return NotFound();
             }  
+        }
+
+        [HttpGet]
+        public ActionResult ExportGrid() {
+            // Using EPPlus from nuget
+            using (ExcelPackage package = new ExcelPackage()) {
+                Int32 row = 2;
+                Int32 col = 1;
+
+                package.Workbook.Worksheets.Add("Data");
+                IGrid<AKCCore.Clipping> grid = CreateExportableGrid();
+                ExcelWorksheet sheet = package.Workbook.Worksheets["Data"];
+
+                foreach (IGridColumn column in grid.Columns) {
+                    sheet.Cells[1, col].Value = column.Title;
+                    sheet.Column(col++).Width = 18;
+                }
+
+                foreach (IGridRow<AKCCore.Clipping> gridRow in grid.Rows) {
+                    col = 1;
+                    foreach (IGridColumn column in grid.Columns)
+                        sheet.Cells[row, col++].Value = column.ValueFor(gridRow);
+
+                    row++;
+                }
+
+                package.Save();
+
+                return File(package.GetAsByteArray(), XlsxContentType, "my_clippings.xlsx");
+            }
+        }
+
+        private IGrid<AKCCore.Clipping> CreateExportableGrid() {
+            List<AKCCore.Clipping> clippingData = Helper.clippingData;
+
+            IGrid<AKCCore.Clipping> grid = new Grid<AKCCore.Clipping>(clippingData);
+            grid.ViewContext = new ViewContext { HttpContext = HttpContext };
+            grid.Query = Request.Query;
+
+            grid.Columns.Add(item => item.Author).Titled("Author");
+            grid.Columns.Add(item => item.BookName).Titled("Book Name");
+            grid.Columns.Add(item => item.ClippingType).Titled("ClippingType");
+            grid.Columns.Add(item => item.DateAdded).Titled("Date Added");
+            grid.Columns.Add(item => item.Location).Titled("Location");
+            grid.Columns.Add(item => item.Text).Titled("Text");
+
+            grid.Pager = new GridPager<AKCCore.Clipping>(grid);
+            grid.Processors.Add(grid.Pager);
+            grid.Pager.RowsPerPage = 10;
+
+            foreach (IGridColumn column in grid.Columns) {
+                column.IsFilterable = true;
+                column.IsSortable = true;
+            }
+
+            return grid;
         }
     }
 }
