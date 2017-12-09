c0515827e5e9fc18b95f3bd4197b5f876e8a3215
Ed Garabito <eduardo@gottabegarabi.com>
2017-12-09 17:34:19 +0000

    Adding icons to Export visible and Export all (temp icons yet).

--- AKCWebCore/wwwroot/js/akc-component-helper.js
+++ AKCWebCore/wwwroot/js/akc-component-helper.js
@@ -5,7 +5,6 @@ var CONTENT_ID_NAME = Object.freeze("AKCContent");
 var body = $body = $("body");
 
 window.onload = function () {
-    //TODO We are not using JQuery where we could (get elemns by id), fix that.
     document.getElementById('startParsingButton').onclick = function() {
         sendParseRequest(undefined, undefined);
     };
